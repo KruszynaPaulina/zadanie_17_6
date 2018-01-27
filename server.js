55e3456b5138162e4a16806ee28dc037c59f5058
Paulina Kruszyna <kruszyna.paulina@gmail.com>
2018-01-27 23:11:29 +0100

    Add task 17.6

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', function(req, res) { 
+    res.render('home');
+});
+
+app.get('/auth/google', function(req, res) {
+	var userName = req.query.username;
+    res.render('hello', {
+    	username: userName
+    });
+});
+
+app.get('/first-template', function(req, res){
+    res.render('first-template');
+});
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
